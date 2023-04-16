950275b50a5f40161335ddd969b6c8187d6d1688
RGNGIT <vnifedow@bk.ru>
2023-04-16 18:02:37 +0500

    +

--- controllers/dictionary.ts
+++ controllers/dictionary.ts
@@ -1,5 +1,38 @@
+import { Request, Response } from "express";
+
 class DictionaryController {
-  
+  async add(req: Request, res: Response): Promise<void> {
+    try {
+      
+    } catch(err) {
+      console.log(err);
+      res.json(err);
+    }
+  }
+  async edit(req: Request, res: Response): Promise<void> {
+    try {
+
+    } catch(err) {
+      console.log(err);
+      res.json(err);
+    }
+  }
+  async delete(req: Request, res: Response): Promise<void> {
+    try {
+
+    } catch(err) {
+      console.log(err);
+      res.json(err);
+    }
+  }
+  async get(req: Request, res: Response): Promise<void> {
+    try {
+
+    } catch(err) {
+      console.log(err);
+      res.json(err);
+    }
+  }
 }
 
 export default new DictionaryController();
